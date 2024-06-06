fb62ff84a34f7d1290b7c4a66ef5b061c38a46e0
CaioCampoReal <engs-caioferreira@camporeal.edu.br>
2024-06-06 19:57:31 -0300

    bloqueio rotas distintas

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
@@ -8,6 +8,7 @@ import Contato from "./pages/Contato";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import useAuth from './components/useAuth';
 import './App.css';
 
 
@@ -22,6 +23,7 @@ function RoutesApp() {
                         <Route path="/sobre" element={<Sobre />} />
                         <Route path="/Lancamentos" element={<Lancamentos />} />
                         <Route path="/Contato" element={<Contato />} />
+                        <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
                 </div>
                 <Footer />
